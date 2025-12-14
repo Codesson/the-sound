d2fe5ff8479bd311678bbe45b54b8498e424ea91
Mato <codessone@gmail.com>
2025-12-15 05:53:52 +0900

    fix: uri 단순화

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/the-sound">
       <div className="App">
         <Header />
         <main className="content">
